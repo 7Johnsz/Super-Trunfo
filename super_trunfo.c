362bed7eb584a080959a2de6882bf90c42f6f228
7johnsz <contatoerro708@gmail.com>
2025-02-27 20:31:16 -0300

    feat: :sparkles: add comparisons for population, area, PIB, points of interest, population density, and PIB per capita in Super Trunfo game

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,12 +4,12 @@ int main() {
     // Valores para carta 1
     char estado[2], codigo_da_carta[5], nome_da_cidade[50];
     int pontos_turisticos, populacao;
-    float area, pib;
+    float area, pib, densidade_populacional, pib_per_capita;
     
     // Valores para carta 2
     char estado2[2], codigo_da_carta2[5], nome_da_cidade2[50];
     int pontos_turisticos2, populacao2;
-    float area2, pib2;
+    float area2, pib2, densidade_populacional2, pib_per_capita2;
 
     // Input dos valores para carta 1
     printf("Seja bem-vindo ao Super Trunfo!\nVamos criar a carta 1!\n\n");
@@ -56,29 +56,72 @@ int main() {
     
     printf("Digite a quantidade de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
-    
-    // Imprimir as cartas
-    printf("\nCarta 1:\n");
-    printf("Estado: %s\n", estado);
-    printf("Código da carta: %s\n", codigo_da_carta);
-    printf("Nome da Cidade: %s\n", nome_da_cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f\n", pib);
-    printf("Pontos turísticos: %d\n", pontos_turisticos);
-    printf("Densidade populacional: %.2f hab/km²\n", populacao/area);
-    printf("PIB per capita: %.2f reais\n", pib/populacao);
 
-    printf("\nCarta 2:\n");
-    printf("Estado: %s\n", estado2);
-    printf("Código da carta: %s\n", codigo_da_carta2);
-    printf("Nome da cidade: %s\n", nome_da_cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f\n", pib2);
-    printf("Pontos turísticos: %d\n", pontos_turisticos2);
-    printf("Densidade populacional: %.2f hab/km²\n", populacao2/area2);
-    printf("PIB per capita: %.2f reais\n", pib2/populacao2);
+    printf("\nComparando as cartas...\n");
+    
+    // Comparação de população
+    printf("População: ");
+    if (populacao > populacao2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (populacao < populacao2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    // Comparação de área
+    printf("Área: ");
+    if (area > area2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (area < area2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    // Comparação de PIB
+    printf("PIB: ");
+    if (pib > pib2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (pib < pib2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    // Comparação de pontos turísticos
+    printf("Pontos turísticos: ");
+    if (pontos_turisticos > pontos_turisticos2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (pontos_turisticos < pontos_turisticos2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    // Comparação de densidade populacional
+    printf("Densidade populacional: ");
+    densidade_populacional = populacao / area;
+    densidade_populacional2 = populacao2 / area2;
+    if (densidade_populacional < densidade_populacional2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (densidade_populacional > densidade_populacional2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    // Comparação de PIB per capita
+    printf("PIB per capita: ");
+    pib_per_capita = pib / populacao;
+    pib_per_capita2 = pib2 / populacao2;
+    if (pib_per_capita < pib_per_capita2) {
+        printf("Carta 1 vence! (1)\n");
+    } else if (pib_per_capita > pib_per_capita2) {
+        printf("Carta 2 vence! (0)\n");
+    } else {
+        printf("Empate!\n");
+    }
 
     return 0;
 }
